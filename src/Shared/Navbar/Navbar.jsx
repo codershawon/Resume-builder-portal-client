65b6a2b8a83a286737c9105c4554d758be883bff
HaqueArif <haquearif143@gmail.com>
2023-08-08 13:49:21 +0600

    nav bar have one more comment

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Navbar = () => {
 Sakib
   return <div></div>;
-=======
+
     return (
         <div>
-            <h2>This is Nav route</h2>
+            {/* <h2>This is Nav route</h2> */}
 =======
             <h1>this in nav</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero et beatae officiis exercitationem consectetur odit iste quidem, omnis corporis velit!</p>
