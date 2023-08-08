50bebb38a59967bb6855fd18325b5d5dabfe5a56
mkhm-programmer <mhkm.programmer@gmail.com>
2023-08-08 12:39:53 +0600

    Done

---
+++ src/components/Demo1.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demo1 = () => {
+    return (
+        <div>
+            <h1>Hello World!!!!!!</h1>
+        </div>
+    );
+};
+
+export default Demo1;
---
+++ src/components/Demo333.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demo333 = () => {
+    return (
+        <div>
+            <h1>This Demo Content</h1>
+        </div>
+    );
+};
+
+export default Demo333;
