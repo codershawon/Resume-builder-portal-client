5b99ad784843d052f1f72794580d9d021c047c59
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-08-08 11:57:02 +0600

    deteled text file

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,16 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const Navbar = () => {
-    return (
-        <div>
-            <h2>This is Nav route</h2>
-            <h2>This is Nav route</h2>
-            <h2>This is Nav route</h2>
-            <h2>This is Nav route</h2>
-            <h2>This is Nav route</h2>
-            <p>This is Nav route</p>
-        </div>
-    );
+  return <div></div>;
 };
 
 export default Navbar;
