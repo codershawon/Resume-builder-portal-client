09593cd0afe6724916538a5955fecd3aa851eb45
Shuvojit Sana <shuvojitsanabd2004@gmai.com>
2023-08-08 12:45:30 +0600

    footer section commited

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -4,6 +4,7 @@ const Footer = () => {
     return (
         <div>
             <h2>This is Footer</h2>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident itaque, ducimus id quisquam enim, blanditiis at deserunt ullam sequi totam molestias. Dicta esse doloremque nemo voluptates iste? Voluptates placeat deserunt, itaque cumque vero mollitia est dolor, asperiores qui in dolores excepturi dolorem, assumenda autem optio nihil eligendi possimus quas dicta?</p>
         </div>
     );
 };
