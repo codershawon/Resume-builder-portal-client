1742adec8c08f6d9d7ede95d273f02e0827e34e8
HaqueArif <haquearif143@gmail.com>
2023-08-08 13:05:47 +0600

    updated nav bar components

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -10,6 +10,7 @@ Sakib
 =======
             <h1>this in nav</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero et beatae officiis exercitationem consectetur odit iste quidem, omnis corporis velit!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid tempora aliquam accusamus soluta deserunt itaque ipsa, necessitatibus laborum, ut autem sapiente at unde vero adipisci facilis eius cum beatae officia blanditiis voluptas. Beatae illum, sapiente quaerat incidunt dolorem fugiat porro repellat, exercitationem maxime hic voluptatem facilis vero earum itaque totam.</p>
 
         </div>
     );
