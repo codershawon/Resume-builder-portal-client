58e7cb5ecf200dcbbc898f2347aaa6f23666b323
Shuvojit Sana <shuvojitsanabd2004@gmai.com>
2023-08-08 19:52:50 +0600

    added a footer part

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -7,6 +7,9 @@ const Footer = () => {
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi quasi ullam cumque quos magni itaque recusandae molestias ea, mollitia eius animi molestiae fugit.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptas dolore dolorem cum fugiat commodi culpa similique modi quaerat cupiditate?</p>
+            <h2>this is footer</h2>
+            <h2>ths is footer</h2>
+            
             
 
         </div>
