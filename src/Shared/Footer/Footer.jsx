93620f4e11f961b28748e4f734d3d110ecf44391
Shuvojit Sana <shuvojitsanabd2004@gmai.com>
2023-08-08 14:31:33 +0600

    footer is commited

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -3,16 +3,11 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <h2>This is Footer</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident itaque, ducimus id quisquam enim, blanditiis at deserunt ullam sequi totam molestias. Dicta esse doloremque nemo voluptates iste? Voluptates placeat deserunt, itaque cumque vero mollitia est dolor, asperiores qui in dolores excepturi dolorem, assumenda autem optio nihil eligendi possimus quas dicta?</p>
-            <h4>This is Footer</h4>
-            <h2>This is Footer</h2>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident itaque, ducimus id quisquam enim, blanditiis at deserunt ullam sequi totam molestias. Dicta esse doloremque nemo voluptates iste? Voluptates placeat deserunt, itaque cumque vero mollitia est dolor, asperiores qui in dolores excepturi dolorem, assumenda autem optio nihil eligendi possimus quas dicta?</p>
-            <h4>This is Footer</h4>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident itaque, ducimus id quisquam enim, blanditiis at deserunt ullam sequi totam molestias. Dicta esse doloremque nemo voluptates iste? Voluptates placeat deserunt, itaque cumque vero mollitia est dolor, asperiores qui in dolores excepturi dolorem, assumenda autem optio nihil eligendi possimus quas dicta?</p>
-            <h4>This is Footer</h4>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident itaque, ducimus id quisquam enim, blanditiis at deserunt ullam sequi totam molestias. Dicta esse doloremque nemo voluptates iste? Voluptates placeat deserunt, itaque cumque vero mollitia est dolor, asperiores qui in dolores excepturi dolorem, assumenda autem optio nihil eligendi possimus quas dicta?</p>
-            <h4>This is Footer</h4>
+            <h2>this is footer</h2>
+            <h2>this is footer</h2>
+            <h2>this is footer</h2>
+            <h2>this is footer</h2>
+            <h2>this is footer</h2>
         </div>
     );
 };
