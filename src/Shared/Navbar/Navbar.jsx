63d041ca4f370787c842a5781da51986150f4dbb
HaqueArif <haquearif143@gmail.com>
2023-08-08 14:09:01 +0600

    added navBar components

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -6,20 +6,15 @@ Sakib
 
     return (
         <div>
-            <h2>This is Nav route</h2>
-
-            <h3>nav nav nav</h3>
-=======
-            <h1>this in nav</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero et beatae officiis exercitationem consectetur odit iste quidem, omnis corporis velit!</p>
-
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid tempora aliquam accusamus soluta deserunt itaque ipsa, necessitatibus laborum, ut autem sapiente at unde vero adipisci facilis eius cum beatae officia blanditiis voluptas. Beatae illum, sapiente quaerat incidunt dolorem fugiat porro , exercitationem maxime hic voluptatem facilis vero earum itaque totam.</p>
-
-            <p>test 01</p>
-=======
-            <p>Lorem ipsum dolor sit amet.</p>
-
-
+            <nav>
+                <ul>
+                    <li>Team Leader</li>
+                    <li>Team Leader</li>
+                    <li>Team Leader</li>
+                    <li>Team Leader</li>
+                    <li>Team Leader</li>
+                </ul>
+            </nav>
         </div>
     );
  main
