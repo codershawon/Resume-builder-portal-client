310b835d3720f351ddc56ae52178ee7de0816ffd
Shuvojit Sana <shuvojitsanabd2004@gmai.com>
2023-08-08 19:58:40 +0600

    added a footet section

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -4,11 +4,6 @@ const Footer = () => {
     return (
         <div>
             <h2>this is footer</h2>
-
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi sequi quasi ullam cumque quos magni itaque recusandae molestias ea, mollitia eius animi molestiae fugit.</p>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum voluptas dolore dolorem cum fugiat commodi culpa similique modi quaerat cupiditate?</p>
-            <h2>this is footer</h2>
-            <h2>ths is footer</h2>
             
             
 
