fd65663fe21f9896960f0759db5bc168bde52449
HaqueArif <haquearif143@gmail.com>
2023-08-08 12:43:22 +0600

    added paragraph

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
             <h2>This is Nav route</h2>
 =======
             <h1>this in nav</h1>
-            {/* <p></p> */}
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero et beatae officiis exercitationem consectetur odit iste quidem, omnis corporis velit!</p>
 
         </div>
     );
