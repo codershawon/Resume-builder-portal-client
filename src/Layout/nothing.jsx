cfc48aee0a27070c58f0899bc5be81a9b4b9af7a
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-08-08 14:05:44 +0600

    nothing commponents

---
+++ src/Layout/nothing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const nothing = () => {
+    return (
+        <div>
+            <h2>this is nothing route</h2>
+        </div>
+    );
+};
+
+export default nothing;
