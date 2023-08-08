763a34851b7fb6a9ee9ea68a3e08a293098d0431
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-08-08 11:01:46 +0600

    last check

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -4,6 +4,11 @@ const Navbar = () => {
     return (
         <div>
             <h2>This is Nav route</h2>
+            <h2>This is Nav route</h2>
+            <h2>This is Nav route</h2>
+            <h2>This is Nav route</h2>
+            <h2>This is Nav route</h2>
+            <p>This is Nav route</p>
         </div>
     );
 };
