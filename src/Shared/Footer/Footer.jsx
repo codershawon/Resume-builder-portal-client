6c145eaebcc9c8e05693e4d097e233b74045bed1
Shuvojit Sana <shuvojitsanabd2004@gmai.com>
2023-08-08 14:15:00 +0600

    added a footer commited

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -9,6 +9,10 @@ const Footer = () => {
             <h2>This is Footer</h2>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident itaque, ducimus id quisquam enim, blanditiis at deserunt ullam sequi totam molestias. Dicta esse doloremque nemo voluptates iste? Voluptates placeat deserunt, itaque cumque vero mollitia est dolor, asperiores qui in dolores excepturi dolorem, assumenda autem optio nihil eligendi possimus quas dicta?</p>
             <h4>This is Footer</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident itaque, ducimus id quisquam enim, blanditiis at deserunt ullam sequi totam molestias. Dicta esse doloremque nemo voluptates iste? Voluptates placeat deserunt, itaque cumque vero mollitia est dolor, asperiores qui in dolores excepturi dolorem, assumenda autem optio nihil eligendi possimus quas dicta?</p>
+            <h4>This is Footer</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident itaque, ducimus id quisquam enim, blanditiis at deserunt ullam sequi totam molestias. Dicta esse doloremque nemo voluptates iste? Voluptates placeat deserunt, itaque cumque vero mollitia est dolor, asperiores qui in dolores excepturi dolorem, assumenda autem optio nihil eligendi possimus quas dicta?</p>
+            <h4>This is Footer</h4>
         </div>
     );
 };
