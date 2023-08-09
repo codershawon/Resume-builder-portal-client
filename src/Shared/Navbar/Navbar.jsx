dfc639ed151f54e9415fe865315bd8d0d4c0102b
mkhm-programmer <mhkm.programmer@gmail.com>
2023-08-09 19:41:52 +0600

    Added a testimonial by static data and also implement dynamic data when get from backend

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Navbar = () => {
-Sakib
+
   return <div></div>;
-=======
+
     return (
         <div>
             <h2>This is Nav route</h2>
-=======
+
             <h1>this in nav</h1>
 
         </div>
