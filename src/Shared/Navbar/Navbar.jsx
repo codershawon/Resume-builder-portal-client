13affbddbdae82e4fb44ed569b7ca10f7d412df7
sakibhasan997 <sssakibhasan997@gmail.comm>
2023-08-09 23:57:09 +0600

    I will add step section and is done now, sakib

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,28 +1,25 @@
 import React from "react";
 
 const Navbar = () => {
-Sakib
-  return <div></div>;
 
-    return (
-        <div>
-            <h2>This is Nav route</h2>
+//     return (
+//         <div>
+//             <h2>This is Nav route</h2>
 
-            <h3>nav nav nav</h3>
-=======
-            <h1>this in nav</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero et beatae officiis exercitationem consectetur odit iste quidem, omnis corporis velit!</p>
+//             <h3>nav nav nav</h3>
+// =======
+//             <h1>this in nav</h1>
+//             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero et beatae officiis exercitationem consectetur odit iste quidem, omnis corporis velit!</p>
 
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid tempora aliquam accusamus soluta deserunt itaque ipsa, necessitatibus laborum, ut autem sapiente at unde vero adipisci facilis eius cum beatae officia blanditiis voluptas. Beatae illum, sapiente quaerat incidunt dolorem fugiat porro  exercitationem maxime  voluptatem facilis vero earum itaque totam.</p>
+//             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid tempora aliquam accusamus soluta deserunt itaque ipsa, necessitatibus laborum, ut autem sapiente at unde vero adipisci facilis eius cum beatae officia blanditiis voluptas. Beatae illum, sapiente quaerat incidunt dolorem fugiat porro  exercitationem maxime  voluptatem facilis vero earum itaque totam.</p>
 
-            <p>test 01</p>
-=======
-            <p>Lorem ipsum dolor sit amet.</p>
+//             <p>test 01</p>
+// =======
+//             <p>Lorem ipsum dolor sit amet.</p>
 
 
-        </div>
-    );
- main
+//         </div>
+//     );
 };
 
 export default Navbar;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Testimonial from '../Testimonial/Testimonial';
+import StepsSection from '../../../components/StepsSection';
 
 const Home = () => {
     return (
         <div>
             {/* Banner section */}
-            <Banner/>
-            <Testimonial/>
+            {/* <Banner/> */}
+            {/* <Testimonial/> */}
+            <StepsSection/>
         </div>
     );
 };
