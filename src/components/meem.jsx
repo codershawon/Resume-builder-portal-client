5c4448523e3bbc0d28807c848a80a69058cd1ada
sabitameem <sabitameem578@gmail.com>
2023-08-10 10:53:19 +0530

    changed from sabita

---
+++ src/components/meem.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const meem = () => {
+    return (
+        <div>
+            <h3>Meem</h3>
+            <h3>Meem</h3>
+        </div>
+    );
+};
+
+export default meem;
