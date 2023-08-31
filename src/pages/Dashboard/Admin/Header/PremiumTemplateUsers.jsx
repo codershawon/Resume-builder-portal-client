46f1a0594ba5b47e397b08749c88fdaa1521e6b2
HaqueArif <haquearif143@gmail.com>
2023-08-31 14:55:19 +0600

    changes design

--- src/pages/Dashboard/Admin/Header/PremiumTemplateUsers.jsx
+++ src/pages/Dashboard/Admin/Header/PremiumTemplateUsers.jsx
@@ -20,8 +20,8 @@ const PremiumTemplateUsers = () => {
 
     return (
         <div className=' rounded-xl shadow-md shadow-[#42C3E4] border  p-5'>
-            <h4 className='text-gray-500  font-extrabold'>Premium</h4>
-            <h4 className='text-gray-500  font-extrabold'>Template users</h4>
+            <h4 className='text-xs sm:text-base text-gray-500  font-extrabold'>Premium</h4>
+            <h4 className='text-xs sm:text-base text-gray-500  font-extrabold'>Template users</h4>
             <p className='font-extrabold text-[#197685] text-xl md:text-3xl lg:text-5xl'>{totalUniqueUsers}</p>
         </div>
     );
